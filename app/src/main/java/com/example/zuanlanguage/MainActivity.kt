5cd75409d0020e4b6ae0b00f9e54987e5a2b5af4
Patrick00017 <742679653@qq.com>
2020-07-14 16:16:08 +0800

    done: done with bottomNavigationView and attach to navHostFragment in main layout

--- app/src/main/java/com/example/zuanlanguage/MainActivity.kt
+++ app/src/main/java/com/example/zuanlanguage/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.zuanlanguage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _navController = findNavController(R.id.fragment)
+//        val fragmentSet = setOf(R.id.GenarateFragment, R.id.DataFragment, R.id.MineFragment)
+        bottomNav.setupWithNavController(_navController)
     }
 }
