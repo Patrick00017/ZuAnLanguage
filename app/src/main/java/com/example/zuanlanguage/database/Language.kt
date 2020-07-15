8a98bc62dc4750cdddf0339c57925295b6f4b192
Patrick00017 <742679653@qq.com>
2020-07-15 20:07:40 +0800

    add: add Room gradle dependencies and an @Entity.

--- app/src/main/java/com/example/zuanlanguage/database/Language.kt
+++ app/src/main/java/com/example/zuanlanguage/database/Language.kt
@@ -1,4 +1,15 @@
 package com.example.zuanlanguage.database
 
-class Language {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Language")
+data class Language (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "acronym")
+    val acronym: String,
+    @ColumnInfo(name = "meaning")
+    val meaning: String
+)
